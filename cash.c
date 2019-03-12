42e0ad774c324feef3dd557338007fe48c2e9920
tdw221 <32180375+tdw221@users.noreply.github.com>
2019-03-11 20:45:24 -0500

    Create cash.c

---
+++ cash.c
@@ -0,0 +1,64 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    // Define variables
+    float total, cents;
+
+    //Prompt input
+    do
+    {
+        printf("Change: ");
+        total = get_float();
+    }
+    while (total < 0);
+    //convert dollars to cents
+    cents = total * 100;
+
+    //define whole number variable and round to integer
+    //printf("%.0f\n", cents);
+
+    //define coins and quarters
+    int coins = 0, quarters = 25;
+
+    //start greedy algorithm with while loop for quarters
+    while (cents >= 25)
+    {
+        coins++;
+        cents = cents - quarters;
+    }
+
+    //define dimes
+    int dimes = 10;
+
+    //while loop for dimes
+    while (cents >= 10)
+    {
+        coins++;
+        cents = cents - dimes;
+    }
+
+    //define nickels
+    int nickels = 5;
+
+    //while loop for nickels
+    while (cents >= 5)
+    {
+        coins++;
+        cents = cents - nickels;
+    }
+
+    //define pennies
+    int pennies = 1;
+
+    //while loop for pennies
+    while (cents >= 1)
+    {
+        coins++;
+        cents = cents - pennies;
+    }
+
+    printf("%i\n", coins);
+}
